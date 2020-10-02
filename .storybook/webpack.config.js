376a146ecef5c990d9aec14a976668ab0302a9b3
jiko21 <daikikojima0531@icloud.com>
2020-10-02 22:50:16 +0900

    fix config error in storybook webpack

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,26 +1,17 @@
-const path = require('path')
+const path = require('path');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.styl$/,
-        loaders: ["style-loader", "css-loader", "stylus-loader",{
-          loader: 'vuetify-loader',
-          options: {
-            theme: path.resolve(__dirname, '../src/stylus/'),
-            url: true
-          }
-        }],
-        include: path.resolve(__dirname, '../src')
+module.exports = ({ config }) => {
+  config.resolve.alias['@'] = path.resolve(__dirname, '../src');
+  config.module.rules.push({
+    test: /\.styl$/,
+    loaders: ["style-loader", "css-loader", "stylus-loader",{
+      loader: 'vuetify-loader',
+      options: {
+        theme: path.resolve(__dirname, '../src/stylus/'),
+        url: true
       }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      vue: 'vue/dist/vue.esm.js',
-      '@': path.resolve(__dirname, '../src/')
-    }
-  }
-}
+    }],
+    include: path.resolve(__dirname, '../src')
+  });
+  return config;
+};
