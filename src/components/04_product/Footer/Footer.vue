19bf0c037e2661fdcca4ddfe8ad5b5b5ca7844ae
ikarostech <ikarostech@live.jp>
2021-01-23 14:42:12 +0900

    コピーライト情報の更新

--- src/components/04_product/Footer/Footer.vue
+++ src/components/04_product/Footer/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-footer color="primary" dark app>
-        2018-2020 Ryo Asano This program is published on MIT Lisence
-    </v-footer>
+  <v-footer color="primary" dark app>
+    2018-2021 Ryo Asano This program is published on MIT Lisence
+  </v-footer>
 </template>
