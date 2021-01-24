242d9298ed7eb03e76317b7e28ff65aed87e1655
ikarostech <ikarostech@live.jp>
2021-01-24 14:35:53 +0900

    コンポーネントの雛形作成

---
+++ src/components/01_element/ExportButton/Airfoil/AirfoilExportButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on">書き出し</v-btn>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title>dat</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+<script lang="ts">
+import { Vector2 } from "three/src/math/Vector2";
+import Vue, { PropType } from "vue";
+export default Vue.extend({
+  props: {
+    shape: Array as PropType<Vector2[]>,
+  },
+});
+</script>
---
+++ src/components/03_composition/DrawingAirfoil/DrawingAirfoil.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <BezierFoilCanvas @updateShape="updateShape" :radius="5" />
+    <p>C_L = {{ C_L }}</p>
+    <airfoil-export-button :shape="shape"></airfoil-export-button>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import BezierFoilCanvas from "@/components/01_element/DrugableCanvas/BezierFoilCanvas.vue";
+import AirfoilExportButton from "@/components/01_element/ExportButton/Airfoil/AirfoilExportButton.vue";
+
+import { Vector2 } from "three/src/math/Vector2";
+import { CL_ModelService } from "@/model/tensorflow/CL_ModelProvider/CL_ModelService";
+import { AIAirfoilPoint } from "@/model/tensorflow/AIAirfoilPoint/AIAirfoilPoint";
+
+export type DataType = {
+  shape: Vector2[];
+  cl_service: CL_ModelService | null;
+};
+
+export default Vue.extend({
+  name: "App",
+  components: {
+    BezierFoilCanvas,
+    AirfoilExportButton,
+  },
+  data: function (): DataType {
+    return {
+      shape: [],
+      cl_service: null,
+    };
+  },
+  methods: {
+    updateShape: function (shape: Vector2[]): void {
+      this.shape = shape;
+    },
+  },
+  computed: {
+    C_L: function (): number {
+      if (
+        this.shape.length === 0 ||
+        this.cl_service === null ||
+        this.cl_service === undefined
+      ) {
+        return 0;
+      }
+      return this.cl_service.predict(
+        new AIAirfoilPoint(this.shape.map((point) => point.y))
+      );
+    },
+  },
+  mounted: async function () {
+    this.cl_service = await CL_ModelService.newBuild();
+  },
+});
+</script>
