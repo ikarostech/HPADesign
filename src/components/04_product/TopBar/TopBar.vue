a4f7082a42a2119b355514bae364086ecf693038
suite-sing <89433079+suite-sing@users.noreply.github.com>
2022-10-27 05:34:25 +0900

    Material Design Iconsの設定を追加

--- src/components/04_product/TopBar/TopBar.vue
+++ src/components/04_product/TopBar/TopBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-app-bar color="primary" dark app>
+    <v-app-bar dark app>
         <v-app-bar-nav-icon />
         <v-toolbar-title>HPADesign</v-toolbar-title>
     </v-app-bar>
